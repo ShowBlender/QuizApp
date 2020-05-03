bb809c90103ae29a68f22e9e285a12693a4bc50b
Joe McCraw <joe@showblender.com>
2020-05-02 18:17:53 -0700

    test_presentableAnswers_withOneSingleAnswer_mapsAnswer + Empty test

--- QuizApp/ResultsPresenter.swift
+++ QuizApp/ResultsPresenter.swift
@@ -11,9 +11,23 @@ import QuizEngine
 
 struct ResultsPresenter {
   let result: Result<Question<String>, [String]>
+  let correctAnswers: Dictionary<Question<String>, [String]>
   
   var summary: String {
     return "You got \(result.score)/\(result.answers.count) Correct"
+  }
+  var presentableAnswers: [PresentableAnswer] {
+    return result.answers.map { (question, userAnswers) in
+      switch question {
+      case .singleAnswer(let value):
+        return PresentableAnswer(question: value,
+                                 answer: correctAnswers[question]!.first! ,
+                                 wrongAnswer: userAnswers.first!)
+      case .multipleAnswer(let value):
+        return PresentableAnswer(question: "", answer: "", wrongAnswer: nil)
+      }
+      
+    }
   }
 }
 
--- QuizAppTests/ResultsPresenterTest.swift
+++ QuizAppTests/ResultsPresenterTest.swift
@@ -16,15 +16,32 @@ class ResultsPresenterTest: XCTestCase {
   func test_summary_withTwoQuestionsaAndScoreOne_returnSummary() {
     let answers = [Question.singleAnswer("Q1"): ["A1"], Question.multipleAnswer("Q2"): ["A2", "A3"]]
     let result = Result(answers: answers, score: 1)
-    let sut = ResultsPresenter(result: result)
+    let sut = ResultsPresenter(result: result, correctAnswers: [:])
     XCTAssertEqual(sut.summary, "You got 1/2 Correct")
-    XCTAssertNotEqual(sut.summary, "You got 2/2 Correct")
+    XCTAssertNotEqual(sut.summary, "You got 2/2 Correct") 
   }
   func test_summary_withThreeQuestionsaAndScoreTwo_returnSummary() {
     let answers = [Question.singleAnswer("Q1"): ["A1"], Question.multipleAnswer("Q2"): ["A2", "A3"], Question.multipleAnswer("Q3"): ["A4", "A5"]]
     let result = Result(answers: answers, score: 2)
-    let sut = ResultsPresenter(result: result)
+    let sut = ResultsPresenter(result: result, correctAnswers: [:])
     XCTAssertEqual(sut.summary, "You got 2/3 Correct")
     XCTAssertNotEqual(sut.summary, "You got 1/2 Correct")
   }
+  func test_presentableAnswers_emptyScoreZero_isEmpty() {
+    let answers = Dictionary<Question<String>, [String]>()
+    let result = Result(answers: answers, score: 0)
+    let sut = ResultsPresenter(result: result, correctAnswers: [:])
+    XCTAssertTrue(sut.presentableAnswers.isEmpty)
+  }
+  func test_presentableAnswers_withOneSingleAnswer_mapsAnswer() {
+    let answers = [Question.singleAnswer("Q1"): ["A1"]]
+    let correctAnswers = [Question.singleAnswer("Q1"): ["A2"]]
+    let result = Result(answers: answers, score: 0)
+    let sut = ResultsPresenter(result: result, correctAnswers: correctAnswers)
+    XCTAssertEqual(sut.presentableAnswers.count, 1)
+    XCTAssertEqual(sut.presentableAnswers.first!.question, "Q1")
+    XCTAssertEqual(sut.presentableAnswers.first!.answer, "A2")
+    XCTAssertEqual(sut.presentableAnswers.first!.wrongAnswer, "A1")
+  }
+  
 }
